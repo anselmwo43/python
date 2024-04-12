fead3c74136242d2c4b834d3918aadc3ce8f2871
helina wondwosen <wondwosenhelina@gmail.com>
2024-04-12 05:30:10 -0700

    tuple

---
+++ vector.py
@@ -0,0 +1,15 @@
+#lst = (2,3,4,5)
+#lst = list(lst)
+#print(type(list))
+#lst.pop(2)
+#print(lst)
+#print([1,2,3]==[1,3,4])
+tu1=(1,3,5)
+tu2=(2,4,6)
+tu3=()
+for i,j in zip(tu1,tu2):
+
+    print((i,j)+(tu3))
+
+
+
